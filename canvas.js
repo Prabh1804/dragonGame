de2b93c7e330202e8d5aee67e7ae0a767220df43
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-02-02 23:31:11 +0530

    Added Enemies with movement

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 import {terrain, generateTerrain} from "./components/terrain.js";
 import {initEnemies, tickEnemies} from "./components/enemies.js";
 
-const speed = 0.001
+const speed = 0.0005;
 window.addEventListener("load", () => {
 
   let loading = document.querySelector(".loading");
