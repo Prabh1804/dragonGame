01c5f1f2bcad6214e9589bd4cf6f1d8708579eb0
Prabhdeep singh <prabhdeep5singh555@gmail.com>
2024-02-02 11:27:07 +0530

    basic code

--- canvas.js
+++ canvas.js
@@ -1,17 +1,24 @@
 import "./components/terrain.js";
+// let deltaTime = 1;
+// let prevTimestamp = 0;
+// 
+// const step = (timestamp) => {
+  // deltaTime = timestamp - prevTimestamp;
+  // prevTimestamp = timestamp;
+  // requestAnimationFrame(step);
+// }
+ // function animate(){
+	// prevTimestamp = performance.now();
+	// requestAnimationFrame(step);
+ // }
+// animate();
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let deltaTime = 1;
-let prevTimestamp = 0;
-
-const step = (timestamp) => {
-  deltaTime = timestamp - prevTimestamp;
-  prevTimestamp = timestamp;
-  requestAnimationFrame(step);
-}
- function animate(){
-	prevTimestamp = performance.now();
-	requestAnimationFrame(step);
- }
-animate();
+window.addEventListener("load",e=>{
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+	canvas.width=window.innerWidth;
+	canvas.height=window.innerHeight;	
+	ctx.strokeStyle="white";
+	ctx.lineWidth = 5;
+	ctx.strokeRect(10,10,100,100);
+});
