97e3624be455058d5d38c28adc9475f2d4cc6d7a
Prabhdeep singh <prabhdeep5singh555@gmail.com>
2024-02-02 17:03:24 +0530

    idk

--- canvas.js
+++ canvas.js
@@ -7,23 +7,24 @@ window.addEventListener("load", () => {
   let deltaTime = 1;
   let prevTimestamp = 0;
 
+function dragon(){	
 	ctx.strokeStyle="white";
 	ctx.lineWidth = 5;
 	ctx.strokeRect(10,10,100,100);
+}
 
-  const canvasResizeObserver = new ResizeObserver((entries) => {
-    for (const entry of entries) {
-
-      ctx.canvas.width  = window.innerWidth;
-      ctx.canvas.height = window.innerHeight;
-      ctx.clearRect(0, 0, ctx.width, ctx.height);
-    }
-  });
-  canvasResizeObserver.observe(canvas);
+function handleWindowResize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+  window.addEventListener("resize", handleWindowResize);
 
   generateTerrain(ctx.canvas.width / ctx.canvas.height);
+  
   function animate(timestamp){
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    dragon();
     deltaTime = timestamp - prevTimestamp;
     prevTimestamp = performance.now();
     terrain(ctx, deltaTime / 1000, 0.001);
