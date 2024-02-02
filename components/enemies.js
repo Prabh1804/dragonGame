db23fb222f947412a38daff259fb0f1d95685c02
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-02-02 14:29:15 +0530

    Templates for enemies

---
+++ components/enemies.js
@@ -0,0 +1,15 @@
+const enemies = [];
+
+const max_countdown = 1000;
+
+let countdown = max_countdown;
+
+const tick_enemies = (deltatime) => {
+  for (const enemy of enemies) {
+    
+  }
+};
+
+export const enemies = (deltatime) => {
+  
+};
